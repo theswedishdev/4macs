64c2143a51d19e15ac6bcba8125e0662863a11f5
Max Sandelin <themaxsandelin@gmail.com>
2017-09-08 14:31:01 +0200

    Clean up + editorconfig

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 // Amazing 😦
-function draw() {
-  var canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
-
-    ctx.fillRect(25, 25, 100, 100);
-    ctx.clearRect(45, 45, 60, 60);
-    ctx.strokeRect(50, 50, 50, 50);
-  }
-}
-
-draw();
+// function draw() {
+//   var canvas = document.getElementById('canvas');
+//   if (canvas.getContext) {
+//     var ctx = canvas.getContext('2d');
+//
+//     ctx.fillRect(25, 25, 100, 100);
+//     ctx.clearRect(45, 45, 60, 60);
+//     ctx.strokeRect(50, 50, 50, 50);
+//   }
+// }
+//
+// draw();
 
 // Example of how we can do hover over (touch over).
 window.addEventListener('mousemove', (e) => {
