fba9e2242e9b97416e9c7933fa24ce86452f545e
Max Sandelin <themaxsandelin@gmail.com>
2017-09-08 20:22:54 +0200

    First attempt at cursor

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,27 +1,51 @@
 'use strict';
 
+// Temporary variable to store the pointer in focus.
+var focusPointer = void 0;
+
 var playerNodes = document.querySelectorAll('.player');
 for (var i = 0; i < playerNodes.length; i++) {
 
-  playerNodes[i].addEventListener('mousedown', function () {
+  playerNodes[i].addEventListener('mousedown', function (e) {
+    console.log('Started tracking..');
+    paintPointer(e.clientX, e.clientY);
     window.addEventListener('mousemove', movePlayer);
-  });
 
-  playerNodes[i].addEventListener('mouseup', function () {
-    window.removeEventListener('mousemove', movePlayer);
+    window.addEventListener('mouseup', function stopTracking() {
+      console.log('Stopped tracking!');
+      window.removeEventListener('mousemove', movePlayer);
+      window.removeEventListener('mouseup', stopTracking);
+    });
   });
 }
 
-function movePlayer() {}
+function movePlayer(e) {
+  var hoverElement = document.elementFromPoint(e.clientX, e.clientY);
+  if (hoverElement.classList.contains('rail') || hoverElement.classList.contains('stop')) {
+    movePointer(focusPointer, e.clientX, e.clientY);
+    console.log('I am on a ' + hoverElement.classList.value);
+  }
+}
+
+function paintPointer(x, y) {
+  var pointer = document.createElement('div');
+  pointer.classList.add('pointer');
+  pointer.style.top = y - 10 + 'px';
+  pointer.style.left = x - 10 + 'px';
+  document.body.appendChild(pointer);
+  focusPointer = pointer;
+}
+
+function movePointer(pointer, x, y) {
+  pointer.style.top = y - 10 + 'px';
+  pointer.style.left = x - 10 + 'px';
+}
 
 // Example of how we can do hover over (touch over).
-window.addEventListener('mousemove', function (e) {
-  var hoverElement = document.elementFromPoint(e.clientX, e.clientY);
-  if (hoverElement.classList.contains('rail')) {
-    console.log(hoverElement.classList.contains('rail'));
-  }
-});
-
-window.addEventListener('touchmove', function (e) {
-  var hoverElement = document.elementFromPoint(e.clientX, e.clientY);
-});
+// window.addEventListener('mousemove', (e) => {
+//
+// });
+//
+// window.addEventListener('touchmove', (e) => {
+//   const hoverElement = document.elementFromPoint(e.clientX, e.clientY);
+// });
--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,50 @@
+// Temporary variable to store the pointer in focus.
+let focusPointer;
+
 const playerNodes = document.querySelectorAll('.player');
 for (let i = 0; i < playerNodes.length; i++) {
 
-  playerNodes[i].addEventListener('mousedown', () => {
+  playerNodes[i].addEventListener('mousedown', (e) => {
+    console.log('Started tracking..');
+    paintPointer(e.clientX, e.clientY);
     window.addEventListener('mousemove', movePlayer);
-  });
 
-  playerNodes[i].addEventListener('mouseup', () => {
-    window.removeEventListener('mousemove', movePlayer);
+    window.addEventListener('mouseup', function stopTracking() {
+      console.log('Stopped tracking!');
+      window.removeEventListener('mousemove', movePlayer);
+      window.removeEventListener('mouseup', stopTracking);
+    });
   });
 
 }
 
-function movePlayer() {
+function movePlayer(e) {
+  const hoverElement = document.elementFromPoint(e.clientX, e.clientY);
+  if (hoverElement.classList.contains('rail') || hoverElement.classList.contains('stop')) {
+    movePointer(focusPointer, e.clientX, e.clientY);
+    console.log( 'I am on a ' + hoverElement.classList.value );
+  }
+}
 
+function paintPointer(x, y) {
+  const pointer = document.createElement('div');
+  pointer.classList.add('pointer');
+  pointer.style.top = (y - 10) + 'px';
+  pointer.style.left = (x - 10) + 'px';
+  document.body.appendChild(pointer);
+  focusPointer = pointer;
+}
+
+function movePointer(pointer, x, y) {
+  pointer.style.top = (y - 10) + 'px';
+  pointer.style.left = (x - 10) + 'px';
 }
 
 // Example of how we can do hover over (touch over).
-window.addEventListener('mousemove', (e) => {
-  const hoverElement = document.elementFromPoint(e.clientX, e.clientY);
-  if (hoverElement.classList.contains('rail')) {
-    console.log(hoverElement.classList.contains('rail'));
-  }
-});
-
-window.addEventListener('touchmove', (e) => {
-  const hoverElement = document.elementFromPoint(e.clientX, e.clientY);
-});
+// window.addEventListener('mousemove', (e) => {
+//
+// });
+//
+// window.addEventListener('touchmove', (e) => {
+//   const hoverElement = document.elementFromPoint(e.clientX, e.clientY);
+// });
