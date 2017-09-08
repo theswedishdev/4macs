1174f995d6dade7d21e9cb9974ac7b37aff99d85
Max Sandelin <themaxsandelin@gmail.com>
2017-09-08 09:31:27 +0200

    Added example code of how to do the hover thing

--- scripts.js
+++ scripts.js
@@ -10,4 +10,13 @@ function draw() {
   }
 }
 
-draw()
+draw();
+
+// Example of how we can do hover over (touch over).
+window.addEventListener('mousemove', (e) => {
+  const hoverElement = document.elementFromPoint(e.clientX, e.clientY);
+});
+
+window.addEventListener('touchmove', (e) => {
+  const hoverElement = document.elementFromPoint(e.clientX, e.clientY);
+});
