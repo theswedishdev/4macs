1f49a70c767ce20f792078fa02d732b5cb3c2de4
Max Sandelin <themaxsandelin@gmail.com>
2017-09-09 08:22:15 +0200

    Wooh

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,23 +3,26 @@
 var playerNodes = document.querySelectorAll('.player');
 for (var i = 0; i < playerNodes.length; i++) {
 
-  playerNodes[i].addEventListener('mousedown', mouseDownHandler);
+  // playerNodes[i].addEventListener('mousedown', mouseDownHandler);
   playerNodes[i].addEventListener('touchstart', mouseDownHandler);
 }
 
 function mouseDownHandler(e) {
   console.log('Started tracking..');
 
-  var pointer = new Pointer('lul', e.clientX, e.clientY);
-  window.addEventListener('mousemove', mouseMove);
+  var x = e.touches ? e.touches[0].clientX : e.clientX;
+  var y = e.touches ? e.touches[0].clientY : e.clientY;
+
+  var pointer = new Pointer('lul', x, y);
+  // window.addEventListener('mousemove', mouseMove);
   window.addEventListener('touchmove', mouseMove);
 
-  window.addEventListener('mouseup', function stopTracking(e) {
-    console.log('Stopped tracking!');
-    pointer.destroy();
-    window.removeEventListener('mousemove', mouseMove);
-    window.removeEventListener('mouseup', stopTracking);
-  });
+  // window.addEventListener('mouseup', function stopTracking(e) {
+  //   console.log('Stopped tracking!');
+  //   pointer.destroy();
+  //   window.removeEventListener('mousemove', mouseMove);
+  //   window.removeEventListener('mouseup', stopTracking);
+  // });
 
   window.addEventListener('touchend', function stopTracking(e) {
     console.log('Stopped tracking!');
@@ -34,10 +37,13 @@ function mouseDownHandler(e) {
 }
 
 function movePlayer(e) {
+  var x = e.touches ? e.touches[0].clientX : e.clientX;
+  var y = e.touches ? e.touches[0].clientY : e.clientY;
+
   this.hide();
-  var hoverElement = document.elementFromPoint(e.clientX, e.clientY);
+  var hoverElement = document.elementFromPoint(x, y);
   if (hoverElement.classList.contains('rail') || hoverElement.classList.contains('stop') || hoverElement.classList.contains('player')) {
-    this.move(e.clientX, e.clientY);
+    this.move(x, y);
   }
   this.show();
 }
--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,26 @@
 const playerNodes = document.querySelectorAll('.player');
 for (let i = 0; i < playerNodes.length; i++) {
 
-  playerNodes[i].addEventListener('mousedown', mouseDownHandler);
+  // playerNodes[i].addEventListener('mousedown', mouseDownHandler);
   playerNodes[i].addEventListener('touchstart', mouseDownHandler);
 }
 
 function mouseDownHandler(e) {
   console.log('Started tracking..');
 
-  let pointer = new Pointer('lul', e.clientX, e.clientY);
-  window.addEventListener('mousemove', mouseMove);
+  const x = (e.touches) ? e.touches[0].clientX:e.clientX;
+  const y = (e.touches) ? e.touches[0].clientY:e.clientY;
+
+  let pointer = new Pointer('lul', x, y);
+  // window.addEventListener('mousemove', mouseMove);
   window.addEventListener('touchmove', mouseMove);
 
-  window.addEventListener('mouseup', function stopTracking(e) {
-    console.log('Stopped tracking!');
-    pointer.destroy();
-    window.removeEventListener('mousemove', mouseMove);
-    window.removeEventListener('mouseup', stopTracking);
-  });
+  // window.addEventListener('mouseup', function stopTracking(e) {
+  //   console.log('Stopped tracking!');
+  //   pointer.destroy();
+  //   window.removeEventListener('mousemove', mouseMove);
+  //   window.removeEventListener('mouseup', stopTracking);
+  // });
 
   window.addEventListener('touchend', function stopTracking(e) {
     console.log('Stopped tracking!');
@@ -32,10 +35,13 @@ function mouseDownHandler(e) {
 }
 
 function movePlayer(e) {
+  const x = (e.touches) ? e.touches[0].clientX:e.clientX;
+  const y = (e.touches) ? e.touches[0].clientY:e.clientY;
+
   this.hide();
-  const hoverElement = document.elementFromPoint(e.clientX, e.clientY);
+  const hoverElement = document.elementFromPoint(x, y);
   if (hoverElement.classList.contains('rail') || hoverElement.classList.contains('stop') || hoverElement.classList.contains('player')) {
-    this.move(e.clientX, e.clientY);
+    this.move(x, y);
   }
   this.show();
 }
