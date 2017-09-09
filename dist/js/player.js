d7978e424ed25ac88533bbd316cbf64e3a48e212
Max Sandelin <themaxsandelin@gmail.com>
2017-09-09 09:35:47 +0200

    Changed from touch to click

--- dist/js/player.js
+++ dist/js/player.js
@@ -9,37 +9,44 @@ var Player = function Player(color) {
   this.start = document.querySelector('.player.' + color.name);
   this.color = color;
 
-  this.start.addEventListener('touchstart', mouseDownHandler.bind(this));
+  this.start.addEventListener('mousedown', mouseDownHandler.bind(this));
+  // this.start.addEventListener('touchstart', mouseDownHandler.bind(this));
 
   function mouseDownHandler(e) {
     e.preventDefault();
-    console.log('Started tracking..');
+    // console.log('Started tracking..');
 
     var x = e.touches ? e.touches[0].clientX : e.clientX;
     var y = e.touches ? e.touches[0].clientY : e.clientY;
 
     var pointer = new Pointer(this.color, x, y);
-    // window.addEventListener('mousemove', mouseMove);
-    window.addEventListener('touchmove', mouseMove);
+    window.addEventListener('mousemove', mouseMove);
+    // window.addEventListener('touchmove', mouseMove);
 
-    // window.addEventListener('mouseup', function stopTracking(e) {
-    //   console.log('Stopped tracking!');
-    //   pointer.destroy();
-    //   window.removeEventListener('mousemove', mouseMove);
-    //   window.removeEventListener('mouseup', stopTracking);
-    // });
-
-    window.addEventListener('touchend', function stopTracking(e) {
+    window.addEventListener('mouseup', function stopTracking(e) {
       e.preventDefault();
-      console.log('Stopped tracking!');
+      // console.log('Stopped tracking!');
       pointer.destroy();
-      window.removeEventListener('touchmove', mouseMove);
-      window.removeEventListener('touchend', stopTracking);
+      window.removeEventListener('mousemove', mouseMove);
+      window.removeEventListener('mouseup', stopTracking);
 
       stops.forEach(function (stop) {
         stop.node.style.fill = '#353535';
       });
     });
+
+    // window.addEventListener('touchend', function stopTracking(e) {
+    //   e.preventDefault();
+    //   console.log('Stopped tracking!');
+    //   pointer.destroy();
+    //   window.removeEventListener('touchmove', mouseMove);
+    //   window.removeEventListener('touchend', stopTracking);
+    //
+    //
+    //   stops.forEach((stop) => {
+    //     stop.node.style.fill = '#353535';
+    //   });
+    // });
 
     function mouseMove(e) {
       e.preventDefault();
